4637ab7f96a0da1c15a6d9eec47ce700178b9f4d
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-06-15 10:55:04 -0400

    front end development

---
+++ client/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <>
+      Hello World!
+      <Routes>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
