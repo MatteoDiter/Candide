cb4bda98aaf3d8cdbc02f997444445aab7a69122
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-06-27 15:23:52 -0400

    fixed server

--- server/router/router.js
+++ server/router/router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path');
 
 // controllers
 //const controller = require('./controller/mainController.js');
 
 // Serve index.html for all other routes to enable client-side routing
-// router.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, '../client/index.html'));
-//   });
+router.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/index.html'));
+});
 
 module.exports = router;
