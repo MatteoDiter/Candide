90565d0a6e558d842c051ff70296de0644db04cc
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-06-28 18:18:45 -0400

    ts mongo connected

--- server/router/router.ts
+++ server/router/router.ts
@@ -1,14 +1,27 @@
-import express, { Router, Request, Response } from 'express';
+import { Router, Request, Response } from 'express';
 import path from 'path';
 
 // controllers
-import Sample from '../controller/sampleController';
+import sampleController from '../controller/sampleController';
 
-const router: Router = express.Router();
+const router: Router = Router();
 
 // Serve index.html for all other routes to enable client-side routing
 router.get('/', (req: Request, res: Response) => {
-    res.sendFile(path.join(__dirname, '../../client/index.html'));
+  res.sendFile(path.join(__dirname, '../../client/index.html'));
 });
 
-export default router
+// POST // WORKING TESTED ON POSTMAN
+router.post('/add-item', sampleController.addItem, (req: Request, res: Response) => {
+  console.log('item added');
+  res.status(200).json(res.locals);
+  //res.status(200).send('item added');
+});
+
+// GET // WORKING TESTED ON POSTMAN
+router.get('/get-item', sampleController.getItem, (req: Request, res: Response) => {
+  console.log('getting items');
+  res.status(200).json(res.locals);
+});
+
+export default router;
