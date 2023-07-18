5ada7fbaea6b9018d72f136bb7c22abdd2606f34
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-07-18 18:41:31 -0400

    clicker base logic completed

--- client/component/home.tsx
+++ client/component/home.tsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
+import Clicker from "./clicker";
 
 const Home: React.FC = () => {
   return (
     <div>
       <h1>Welcome Home!</h1>
+      <Clicker />
     </div>
   );
 };
