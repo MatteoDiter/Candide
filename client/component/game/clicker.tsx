d827dd6b444e090a1866c810c9b736cb6c3290b6
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-07-20 18:41:51 -0400

    added time logic

--- client/component/game/clicker.tsx
+++ client/component/game/clicker.tsx
@@ -1,15 +1,45 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Clicker: React.FC = () => {
-  const [value, setValue] = useState(10);
+  // set time and clicks
+  const time = 10;
+  const clicks = 10;
 
+  // set states
+  const [value, setValue] = useState(time); // clicks left to beat the game
+  const [timer, setTimer] = useState(clicks); // time left to beat the game
+
+  // setting reset timer
+  const resetTimer = () => {
+    setTimer(time);
+  };
+  // timer logic
+  useEffect(() => {
+    const timeInterval = setInterval(() => {
+      setTimer((prevTimer) => prevTimer - 1);
+    }, 1000);
+
+    return () => clearInterval(timeInterval);
+  }, [value]);
+
+  // game over logic by time
+  useEffect(() => {
+    if (timer === 0) {
+      alert("Time's Up! - Game Over");
+      setValue(10);
+      resetTimer();
+    }
+  }, [timer]);
+
+  // game win logic by clicks
   const handleClick = () => {
     setValue((preValue) => {
       let newValue = preValue - 1;
-      // game over conditional
+      // winning game conditional
       if (newValue === 0) {
-        alert("Game Over");
-        newValue = 10;
+        alert("You Win");
+        newValue = clicks;
+        resetTimer();
       }
       return newValue;
     });
@@ -17,6 +47,7 @@ const Clicker: React.FC = () => {
 
   return (
     <div>
+      <p>Time remaining: {timer} seconds</p>
       <button onClick={handleClick}>clicker</button>
       <p>{value}</p>
     </div>
