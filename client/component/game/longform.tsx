c1aeb2d954e8e065dc6eccd6b7aaf745ad00140d
@MatteoDiter <115022965+MatteoDiter@users.noreply.github.com>
2023-08-23 13:04:37 -0400

    fixed longform3

--- client/component/game/longform.tsx
+++ client/component/game/longform.tsx
@@ -5,3 +5,7 @@ export const longArray1: string[] = [
 export const longArray2: string[] = [
   "Homeless, he joins the army, witnessing the horrors of war. He makes his way to Holland and finds a kind Anabaptist, who takes him away to Lisbon via ship. There, Candide surprisingly finds Cunégonde, who escaped the castle that was attacked during the war. She is living with two powerful men who come upon the young lovers, and Candide kills them both. Frightened, they escape through a vessel loading up for a mission in South America. In Buenos Aires,they meet the governor who takes an interest in Cunégonde.",
 ];
+
+export const longArray3: string[] = [
+  "Heartbroken, Candide cannot fight and must flee. Candide ends up in Eldorado, a country filled with gold for which the citizens have no use because everyone's needs are met. Eager to find Cunégonde, Candide leaves loaded with gold, and other supplies. An informant tells him that Cunégonde is now in Constantinople, working as a servant. Candide sets off to find Cunégonde, during the trip most of his fortune gets lost, but once arrived, he buys her freedom. The two get married and buy a small farm with the last of his Eldorado fortune. Candide decides that this is how they will find happiness, and they begin to work on their farm.",
+];
